b08b98101a8a9609c5de05cdb19894c215a51a2f
MK <46850924+97MK@users.noreply.github.com>
2021-05-04 05:15:49 +0300

    link new login and register

--- api/views.py
+++ api/views.py
@@ -20,98 +20,74 @@ from django.views.decorators.csrf import csrf_exempt
 database = Database()
 
 
-def addP(request):
-	# GET /
-	if request.session.has_key("user"):
-		email = request.session['user']['email']
-		print(f"Signed in as {email}")
-		return render(request, 'Home.html', {"email": email})
-	else:
-		print("You need to login")
-		return redirect("/login/")
-
-
-# def home(request):
-# 		# GET /
-# 		if request.session.has_key("user"):
-# 			email = request.session['user']['email']
-# 			print(f"Signed in as {email}")
-# 			return render(request, 'main.html')
-# 			#return render(request, 'main.html', {"email": email})
-# 		else:
-# 			print("You need to login")
-# 			return redirect("/login/")
-#
 
 @csrf_exempt
 def login(request):
-	# GET /login
-	if request.method == "GET":
-		if request.session.has_key('email'):
-			return JsonResponse({"email": request.session['email']})
-		else:
-			return render(request, 'Login.html')
-
-	# POST /login
-	elif request.method == "POST":
-		input_email = request.POST.get('email')
-		input_password = request.POST.get('password')
-		print(f'Email {input_email}\t\tPassword {input_password}')
-
-		query = """	SELECT * FROM KNOAP.doctor
-					WHERE email = '%s'
-					AND password = '%s';
-		""" % (input_email, input_password)
-		records = database.query(query)
-
-		if type(records) is not JsonResponse:
-			if records['count'] > 0:
-				request.session['user'] = records['records'][0]
-				return redirect("/", {"email": input_email})
+	def login(request):
+		# GET /login
+		if request.method == "GET":
+			if request.session.has_key('email'):
+				return JsonResponse({"email": request.session['email']})
 			else:
-				return JsonResponse({'error': 'Email or password may be incorrect'})
-		return records
+				return render(request, 'Login_register.html')
+
+		# POST /login
+		elif request.method == "POST":
+
+			input_email = request.POST.get('email')
+			input_password = request.POST.get('password')
+			print(f'Email {input_email}\t\tPassword {input_password}')
+
+			query = """	SELECT * FROM KNOAP.doctor
+						WHERE email = '%s'
+						AND password = '%s';
+			""" % (input_email, input_password)
+			records = database.query(query)
+
+			if type(records) is not JsonResponse:
+				if records['count'] > 0:
+					request.session['user'] = records['records'][0]
+					return redirect("/", {"email": input_email})
+				else:
+					return JsonResponse({'error': 'Email or password may be incorrect'})
+			return records
 
 
 def logout(request):
-	# GET /logout
-	if request.method == "GET":
-		try:
-			# print(f"Logging out of {request.session['email']}")
-			del request.session['user']
-			return JsonResponse({"message": "Logged out successfully"})
-		except Exception as e:
-			# print(f"Failed to logout\n{query_str(e)}")
-			return JsonResponse({"error": e})
+    # GET /logout
+    if request.method == "GET":
+        try:
+            # print(f"Logging out of {request.session['email']}")
+            del request.session['user']
+            return redirect("/login/")
 
-
-# return redirect("/login/")
-
+        except Exception as e:
+            # print(f"Failed to logout\n{query_str(e)}")
+            return JsonResponse({"error": e})
 
 @csrf_exempt
 def register(request):
-	# GET /register
-	if request.method == "GET":
-		return render(request, "Register.html")
+    # GET /register
+    if request.method == "GET":
+        return render(request, "login.html")
 
-	# POST /register
-	elif request.method == "POST":
-		input_fname = request.POST.get('firstname')
-		input_lname = request.POST.get('lastname')
-		input_email = request.POST.get('email')
-		input_password = request.POST.get('password')
+    # POST /register
+    elif request.method == "POST":
+        input_fname = request.POST.get('firstname')
+        input_lname = request.POST.get('lastname')
+        input_email = request.POST.get('email')
+        input_password = request.POST.get('password')
 
-		query = """INSERT INTO KNOAP.doctor (fname, lname, email, password,authorized)
+        query = """INSERT INTO KNOAP.doctor (fname, lname, email, password,authorized)
 						VALUES ('%s', '%s', '%s', '%s', '%s')
 						returning *;""" % (input_fname, input_lname, input_email, input_password, 'n')
-		records = database.query(query)
-		if type(records) is not JsonResponse:
-			request.session['user'] = records[0]
-			return JsonResponse({'records': records}, content_type="application/json")
-		return records
+        records = database.query(query)
+        if type(records) is not JsonResponse:
+            return redirect("/login/")
+
+        return records
 
 
-# return render(request, "Login.html")
 
 
 # def reset(request):
@@ -133,48 +109,33 @@ def register(request):
 
 @csrf_exempt
 def add_patient(request):
-	if request.method == "POST":
-		first_name = request.POST.get('fname')
-		last_name = request.POST.get('lname')
-		gender = request.POST.get('gender')
-		birthday = request.POST.get('birthday')
-		city = request.POST.get('city')
-		phone = request.POST.get('phone')
-		street = request.POST.get('street')
-		zip_code = request.POST.get('zipCode')
-		patient_email = request.POST.get('email')
-		date = datetime.date.today()
-		doctor = request.POST.get('doctorEmail')
-		notes = request.POST.get('notes')
+    if request.method == "POST":
+        first_name = request.POST.get('fname')
+        last_name = request.POST.get('lname')
+        gender = request.POST.get('gender')
+        birthday = request.POST.get('birthday')
+        city = request.POST.get('city')
+        phone = request.POST.get('phone')
+        street = request.POST.get('street')
+        zip_code = request.POST.get('zipCode')
+        patient_email = request.POST.get('email')
+        date = datetime.date.today()
+        doctor = request.session['user']['id']
+        notes = ""
+        grade = 5
 
-		data = {'firstName': first_name,
-				'lastName': last_name,
-				'gender': gender,
-				'birthday': birthday,
-				'city': city,
-				'phone': phone,
-				'street': street,
-				'zipCode': zip_code,
-				'email': patient_email,
-				'date': date,
-				'doctorEmail': doctor,
-				'notes': notes}
+        query = """INSERT INTO KNOAP.patient (fname, lname, gender, birthday, phone, street, city, zipcode, email, registered, notes, assigned_doctor, last_activity,grade)
+								VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s','%s')
+								RETURNING *;""" % (first_name, last_name, gender, birthday, phone, street, city, zip_code, patient_email, date, notes, doctor,date,grade)
+        records = database.query(query)
+        if type(records) is not JsonResponse:
+           return redirect("/")
 
-		query = """INSERT INTO KNOAP.patient (fname, lname, gender, birthday, phone, street, city, zipcode, email, registered, notes, assigned_doctor, last_activity)
-				VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s','%s')
-				RETURNING *;""" % (
-		first_name, last_name, gender, birthday, phone, street, city, zip_code, patient_email, date, notes, doctor,
-		date)
+    # return JsonResponse(data)
+    # return render(request, "Home.html")
+    # return render(request, "Home.html", {"message": "Could not add patient"})
+    return render(request, 'add_patient.html')
 
-		records = database.query(query)
-		if type(records) is not JsonResponse:
-			return JsonResponse({'records': records}, content_type="application/json")
-	# return records
-
-	# return JsonResponse(data)
-	# return render(request, "Home.html")
-	# return render(request, "Home.html", {"message": "Could not add patient"})
-	return JsonResponse({"message": "Could not add patient"})
 
 
 def list_all_doctors(request):
